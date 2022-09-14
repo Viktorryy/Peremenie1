cb34d9f1719e97e054a4b2b68b4cacfe487a775a
Viktorryy <viktory_o@rambler.ru>
2022-09-14 18:46:14 +0400

    задача 8
    ошибочно задача 8 слилась с задачей 7

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        var dog = 8;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
+        var hourAll = 640;
+        var sotrOneHour = 8;
+        var sotrAll = hourAll / sotrOneHour;
+        System.out.println("Всего работников в компании – " + sotrAll + " человек.");
+
+        sotrAll =  sotrAll + 94;
+        var hourAllNew = sotrAll * 8;
+        System.out.println("Если в компании работает " + sotrAll + " человека, " +
+                "то всего " + hourAllNew + " часов работы " +
+                "может быть поделено между сотрудниками.");
+
+        var boxerRazn = boxerTwo - boxerOne;
+        System.out.println(boxerRazn);
+
+        var boxerOst = boxerTwo % boxerOne;
+        System.out.println(boxerOst);
     }
 }
