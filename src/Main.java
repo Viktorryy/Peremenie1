f83cb980121f8f496960879a4ff5199e333b4e8a
Viktorryy <viktory_o@rambler.ru>
2022-09-14 18:06:44 +0400

    задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        var dog = 8;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
+        var boxerOne = 78.2;
+        System.out.println(boxerOne);
+        var boxerTwo = 82.7;
+        System.out.println(boxerTwo);
+        var boxerSum = boxerOne + boxerTwo;
+        System.out.println(boxerSum);
+        var boxerRazn = boxerTwo - boxerOne;
+        System.out.println(boxerRazn);
+
     }
 }
